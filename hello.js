b24aecd016434722824fd1c6ffe14fa0d9711d5f
Peyman Borhani <peyman.borhani@gmail.com>
2021-06-16 17:54:39 +0000

    testing remote commit

--- hello.js
+++ hello.js
@@ -1,9 +1,19 @@
-import { serve } from './deps.js'
-import getIndex from './api/mod.js'
-import getAbout from './api/about.jsx'
+/*
+import { serve }  from  './deps.js';
+import  getIndex  from  './api/mod.js';
+import  getAbout  from  './api/about.jsx';
 
 
-serve({
-  '/': getIndex,
-  '/about': getAbout
-})
+serve(  {
+          '/'      :   getIndex,
+          '/about' :   getAbout
+        }
+); */
+
+addEventListener('fetch', (event) => {
+    const response =  new Response( 'Hello Gitpod', 
+                                    { headers: {'Content-Type': 'text/plain'} }
+                      );
+
+  event.respondWith(response);
+});
