1ac1d3eb9e7e52211ede8d8415e18d76ee82c0f5
Peyman Borhani <peyman.borhani@gmail.com>
2021-06-21 19:23:25 +0000

    code updated

--- deps.js
+++ deps.js
@@ -1,2 +1,8 @@
 
 export  {serve, json}  from  "https://deno.land/x/sift@0.3.2/mod.ts";
+
+/*
+deno install --allow-read --allow-write --allow-env --allow-net --allow-run --no-check -f https://deno.land/x/deploy/deployctl.ts
+
+deployctl --libs=ns,fetchevent  run ./hello.js
+*/
