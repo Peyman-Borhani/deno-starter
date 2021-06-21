e6541e5140c9119337a984adbb4d15723d5ea526
Peyman Borhani <peyman.borhani@gmail.com>
2021-06-21 16:27:24 +0000

    code updated

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ import { serve }  from  './deps.js';
 
 
 import  getIndex  from  './api/mod.js';
-import  getAbout  from  './api/about.jsx';
+import  getAbout  from  './api/about.js';
 
 
 serve(  {
